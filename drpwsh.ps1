48c0481c139c619f0b6c9e9e4cbc0e498e2cdd2b
vinicosta2204 <106935211+vinicosta2204@users.noreply.github.com>
2022-06-05 20:20:59 -0300

    Add files via upload

---
+++ drpwsh.ps1
@@ -0,0 +1,161 @@
+$vault = New-AzRecoveryServicesVault -Name "asrVault" -ResourceGroupName "viniASR" -Location "West US 2"
+
+Set-AzRecoveryServicesAsrVaultContext -Vault $vault
+
+$TempASRJob = New-AzRecoveryServicesAsrFabric -Azure -Location 'East US'  -Name "A2Ademo-EastUS"
+
+while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")){
+        sleep 10;
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+}
+
+Write-Output $TempASRJob.State
+
+$PrimaryFabric = Get-AzRecoveryServicesAsrFabric -Name "A2Ademo-EastUS"
+
+
+#Create Recovery ASR fabric
+$TempASRJob = New-AzRecoveryServicesAsrFabric -Azure -Location 'West US 2'  -Name "A2Ademo-WestUS"
+
+while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")){
+        sleep 10;
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+}
+
+Write-Output $TempASRJob.State
+
+$RecoveryFabric = Get-AzRecoveryServicesAsrFabric -Name "A2Ademo-WestUS"
+
+#Create a Protection container in the primary Azure region (within the Primary fabric)
+$TempASRJob = New-AzRecoveryServicesAsrProtectionContainer -InputObject $PrimaryFabric -Name "A2AEastUSProtectionContainer"
+
+while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")){
+        sleep 10;
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+}
+
+Write-Output $TempASRJob.State
+
+$PrimaryProtContainer = Get-AzRecoveryServicesAsrProtectionContainer -Fabric $PrimaryFabric -Name "A2AEastUSProtectionContainer"
+
+#Create a Protection container in the recovery Azure region (within the Recovery fabric)
+$TempASRJob = New-AzRecoveryServicesAsrProtectionContainer -InputObject $RecoveryFabric -Name "A2AWestUSProtectionContainer"
+
+while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")){
+        sleep 10;
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+}
+
+Write-Output $TempASRJob.State
+
+$RecoveryProtContainer = Get-AzRecoveryServicesAsrProtectionContainer -Fabric $RecoveryFabric -Name "A2AWestUSProtectionContainer"
+
+#Create replication policy
+$TempASRJob = New-AzRecoveryServicesAsrPolicy -AzureToAzure -Name "A2APolicy" -RecoveryPointRetentionInHours 24 -ApplicationConsistentSnapshotFrequencyInHours 4
+
+while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")){
+        sleep 10;
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+}
+
+Write-Output $TempASRJob.State
+
+$ReplicationPolicy = Get-AzRecoveryServicesAsrPolicy -Name "A2APolicy"
+
+#Create Protection container mapping between the Primary and Recovery Protection Containers with the Replication policy
+$TempASRJob = New-AzRecoveryServicesAsrProtectionContainerMapping -Name "A2APrimaryToRecovery" -Policy $ReplicationPolicy -PrimaryProtectionContainer $PrimaryProtContainer -RecoveryProtectionContainer $RecoveryProtContainer
+
+while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")){
+        sleep 10;
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+}
+
+Write-Output $TempASRJob.State
+
+$EusToWusPCMapping = Get-AzRecoveryServicesAsrProtectionContainerMapping -ProtectionContainer $PrimaryProtContainer -Name "A2APrimaryToRecovery"
+
+#Create Protection container mapping (for fail back) between the Recovery and Primary Protection Containers with the Replication policy
+$TempASRJob = New-AzRecoveryServicesAsrProtectionContainerMapping -Name "A2ARecoveryToPrimary" -Policy $ReplicationPolicy -PrimaryProtectionContainer $RecoveryProtContainer -RecoveryProtectionContainer $PrimaryProtContainer
+
+while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")){
+        sleep 10;
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+}
+
+Write-Output $TempASRJob.State
+
+
+$WusToEusPCMapping = Get-AzRecoveryServicesAsrProtectionContainerMapping -ProtectionContainer $RecoveryProtContainer -Name "A2ARecoveryToPrimary"
+
+$EastUSCacheStorageAccount =Get-AzStorageAccount -ResourceGroupName "viniciusRG" -Name "vinicacheasrtest01"
+
+$WestUSTargetStorageAccount = Get-AzStorageAccount -ResourceGroupName "viniASR" -Name "vinitargetasrtest02"
+
+$vm = Get-AzVM | Where-Object {$_.Tags.Keys -eq "swoDR" -and $_.Tags.Values -eq "1"}
+
+#Network Mapping
+
+$WestUSRecoveryVnet = New-AzVirtualNetwork -Name "asrVnet" -ResourceGroupName "viniASR" -Location 'West US 2' -AddressPrefix "10.0.0.0/16"
+
+Add-AzVirtualNetworkSubnetConfig -Name "default" -VirtualNetwork $WestUSRecoveryVnet -AddressPrefix "10.0.0.0/20" | Set-AzVirtualNetwork
+
+$WestUSRecoveryNetwork = $WestUSRecoveryVnet.Id
+
+$SplitNicArmId = $VM.NetworkProfile.NetworkInterfaces[0].Id.split("/")
+
+$NICRG = $SplitNicArmId[4]
+
+$NICname = $SplitNicArmId[-1]
+
+$NIC = Get-AzNetworkInterface -ResourceGroupName $NICRG -Name $NICname
+
+$PrimarySubnet = $NIC.IpConfigurations[0].Subnet
+
+$EastUSPrimaryNetwork = (Split-Path(Split-Path($PrimarySubnet.Id))).Replace("\","/")
+
+#Create an ASR network mapping between the primary Azure virtual network and the recovery Azure virtual network
+$TempASRJob = New-AzRecoveryServicesAsrNetworkMapping -AzureToAzure -Name "A2AEusToWusNWMapping" -PrimaryFabric $PrimaryFabric -PrimaryAzureNetworkId $EastUSPrimaryNetwork -RecoveryFabric $RecoveryFabric -RecoveryAzureNetworkId $WestUSRecoveryNetwork
+
+while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")){
+        sleep 10;
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+}
+
+Write-Output $TempASRJob.State
+
+#Create an ASR network mapping for fail back between the recovery Azure virtual network and the primary Azure virtual network
+$TempASRJob = New-AzRecoveryServicesAsrNetworkMapping -AzureToAzure -Name "A2AWusToEusNWMapping" -PrimaryFabric $RecoveryFabric -PrimaryAzureNetworkId $WestUSRecoveryNetwork -RecoveryFabric $PrimaryFabric -RecoveryAzureNetworkId $EastUSPrimaryNetwork
+
+while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")){
+        sleep 10;
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+}
+
+Write-Output $TempASRJob.State
+
+#Replicate Machine
+
+$RecoveryRG = Get-AzResourceGroup -Name "viniASR" -Location "West US 2"
+
+$OSdiskId = $vm.StorageProfile.OsDisk.ManagedDisk.Id
+$RecoveryOSDiskAccountType = $vm.StorageProfile.OsDisk.ManagedDisk.StorageAccountType
+$RecoveryReplicaDiskAccountType = $vm.StorageProfile.OsDisk.ManagedDisk.StorageAccountType
+
+$OSDiskReplicationConfig = New-AzRecoveryServicesAsrAzureToAzureDiskReplicationConfig -ManagedDisk -LogStorageAccountId $EastUSCacheStorageAccount.Id `
+         -DiskId $OSdiskId -RecoveryResourceGroupId  $RecoveryRG.ResourceId -RecoveryReplicaDiskAccountType  $RecoveryReplicaDiskAccountType `
+         -RecoveryTargetDiskAccountType $RecoveryOSDiskAccountType
+
+
+$datadiskId1 = $vm.StorageProfile.DataDisks[0].ManagedDisk.Id
+$RecoveryReplicaDiskAccountType = $vm.StorageProfile.DataDisks[0].ManagedDisk.StorageAccountType
+$RecoveryTargetDiskAccountType = $vm.StorageProfile.DataDisks[0].ManagedDisk.StorageAccountType
+
+$DataDisk1ReplicationConfig  = New-AzRecoveryServicesAsrAzureToAzureDiskReplicationConfig -ManagedDisk -LogStorageAccountId $EastUSCacheStorageAccount.Id `
+         -DiskId $datadiskId1 -RecoveryResourceGroupId $RecoveryRG.ResourceId -RecoveryReplicaDiskAccountType $RecoveryReplicaDiskAccountType `
+         -RecoveryTargetDiskAccountType $RecoveryTargetDiskAccountType
+
+$diskconfigs = @()
+$diskconfigs += $OSDiskReplicationConfig, $DataDisk1ReplicationConfig
+
+$TempASRJob = New-AzRecoveryServicesAsrReplicationProtectedItem -AzureToAzure -AzureVmId $vm.Id -Name (New-Guid).Guid -ProtectionContainerMapping $EusToWusPCMapping -AzureToAzureDiskReplicationConfiguration $diskconfigs -RecoveryResourceGroupId $RecoveryRG.ResourceId
+
